de892f8c3376363dafcedff81c739e137b8913c4
Yuya Sasaki <63325605+ysasak-t@users.noreply.github.com>
2020-04-28 12:21:08 +0900

    add privacy policy

---
+++ pagesrc/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+body {
+  background: #eaeaea;
+  color: #222;
+  font-size: 14px;
+}
+
+h1 {
+  font-size: 30px;
+  margin: 2em 0 0.5em 0;
+}
+
+h2 {
+  font-size: 20px;
+  margin: 1em 0 0.5em 0;
+}
+</style>
---
+++ pagesrc/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class PagesIndex extends Vue {}
+</script>
+
+<style></style>
